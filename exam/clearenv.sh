4fcff2d8bfb1e9d09fb7cfa53a3701d0d7e9d2f4
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-20 20:46:09 +0800

    Create clearenv.sh

---
+++ exam/clearenv.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+cluster=$(kubectl config view |grep 'cluster: ' | cut -d ':' -f 2)
+rm *.yaml && echo 'remove all yaml' & >/dev/null
+### Pod Logs (5%)
+[ -f /opt/KUTR00101/bar ] && rm /opt/KUTR00101/bar
+echo 'Pod Logs clear'
+
+bash -c "kubectl get pod > getpod.tmp"
+bash -c "cat getpod.tmp |tail -n +2|cut -d ' ' -f 1 > rmpod.tmp"
+
+while read pod
+do
+  kubectl delete pod ${pod}
+
+done < rmpod.tmp
+rm getpod.tmp rmpod.tmp
+
+echo 'apiVersion: v1
+kind: Pod
+metadata:
+  name: bar
+spec:
+  containers:
+  - name: bar-container
+    image: quay.io/cloudwalker/alpine
+    command: ["/bin/sh"]
+    args:
+    - -c
+    - |
+      mkdir -p /var/log
+      while true;
+      do
+        echo "unable-to-access-website"
+        sleep 10
+      done' > pod-log.yaml
+kubectl replace -f pod-log.yaml --force
+
+### Check Ready Node (4%)
+[ -f /opt/KUSC00402/ ] && rm /opt/KUSC00402/*
+echo 'Check Ready Node clear'
+
+### NodeSelector (4%)
+kubectl label node ${cluster}-worker disk=spinning
+### CPU (5%)
+[ -d /opt/KUTR00401/ ] && rm /opt/KUTR00401/* && touch /opt/KUTR00401/KURT00401.txt
+### Sidecar (7%)
+echo $'apiVersion: v1
+kind: Pod
+metadata:
+  name: big-corp-app
+spec:
+  containers:
+  - name: pod-sidecar
+    image: quay.io/cloudwalker/alpine
+    command: ["/bin/sh"]
+    args:
+    - -c
+    - |
+      mkdir -p /var/log;i=0;
+      while true;
+      do
+        echo "$(date) INFO $i" >> /var/log/big-corp-app.log;
+        i=$((i+1));sleep 1;
+      done
+' > pod-sidecar.yaml
+
+kubectl create -f pod-sidecar.yaml
