6f457bae2d4bf44b97ad8e6a407f59acfde58c5e
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-20 21:50:03 +0800

    Update clearenv.sh

--- exam/clearenv.sh
+++ exam/clearenv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cluster=$(kubectl config view |grep 'cluster: ' | cut -d ':' -f 2)
-rm *.yaml && echo 'remove all yaml' & >/dev/null
+rm *.yaml && echo 'remove all yaml' 
 ### Pod Logs (5%)
-[ -f /opt/KUTR00101/bar ] && rm /opt/KUTR00101/bar
+[ ! -d /opt/KUTR00101 ] && mkdir -p /opt/KUTR00101
+[ -f /opt/KUTR00101/bar ] && rm /opt/KUTR00101/*
 echo 'Pod Logs clear'
 
-bash -c "kubectl get pod > getpod.tmp"
-bash -c "cat getpod.tmp |tail -n +2|cut -d ' ' -f 1 > rmpod.tmp"
+kubectl get pod | tail -n +2 | cut -d ' ' -f 1 > rmpod.tmp
 
 while read pod
 do
@@ -33,16 +33,24 @@ spec:
         echo "unable-to-access-website"
         sleep 10
       done' > pod-log.yaml
-kubectl replace -f pod-log.yaml --force
+kubectl apply -f pod-log.yaml
 
 ### Check Ready Node (4%)
-[ -f /opt/KUSC00402/ ] && rm /opt/KUSC00402/*
+
+[ ! -d /opt/KUSC00402/ ] && mkdir -p /opt/KUSC00402/
+[ -d /opt/KUSC00402/ ] && rm /opt/KUSC00402/*
 echo 'Check Ready Node clear'
 
 ### NodeSelector (4%)
 kubectl label node ${cluster}-worker disk=spinning
 ### CPU (5%)
 [ -d /opt/KUTR00401/ ] && rm /opt/KUTR00401/* && touch /opt/KUTR00401/KURT00401.txt
+kubectl label node ${cluster}-worker2 name=cpu-loader
+
+
+kubectl run cpu1 --image=quay.io/cloudwalker/sre.base --restart=Never --labels=name=cpu-loader -- stress-ng --cpu 2  --timeout 600s
+
+
 ### Sidecar (7%)
 echo $'apiVersion: v1
 kind: Pod
