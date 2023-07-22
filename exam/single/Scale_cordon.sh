ca33be0a454cae5dcf54cefa9e9f25883d979397
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-22 18:18:54 +0800

    Create Scale_cordon.sh

---
+++ exam/single/Scale_cordon.sh
@@ -0,0 +1,2 @@
+curl -s 'https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/single/Deployment-Scale.sh' | bash
+curl -s 'https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/single/cordon_drain.sh'|bash
