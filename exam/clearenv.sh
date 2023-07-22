c44f96c194f78dc40260c4d3587bc413c8cbb6af
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-23 02:11:36 +0800

    Update clearenv.sh

--- exam/clearenv.sh
+++ exam/clearenv.sh
@@ -44,8 +44,8 @@ kubectl create -f https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/y
 
 ## Deployment 應用 - Scale (4%)
 
-kubectl delete deploy presentation 2> dev/null;
-kubectl delete deploy loadbalance 2> dev/null;
+kubectl delete deploy presentation 2> /dev/null;
+kubectl delete deploy loadbalance 2> /dev/null;
 
 kubectl create deployment presentation --image=registry.k8s.io/echoserver:1.10 --replicas=0 2> dev/null;
 kubectl create deployment loadbalance --image=registry.k8s.io/echoserver:1.10 --replicas=0 2> dev/null;
