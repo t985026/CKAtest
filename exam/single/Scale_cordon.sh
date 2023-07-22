bdc11740590a93b2661201f8cc44bcb9cac0d9a4
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-22 18:22:31 +0800

    Update Scale_cordon.sh

--- exam/single/Scale_cordon.sh
+++ exam/single/Scale_cordon.sh
@@ -1,4 +1,5 @@
-
-
+##Deployment-Scale.sh
 curl -s 'https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/single/Deployment-Scale.sh' | bash
+
+##cordon_drain.sh
 curl -s 'https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/single/cordon_drain.sh'|bash
