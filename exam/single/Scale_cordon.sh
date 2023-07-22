25ca931ba66351fcc38ed67ab8e32b0a76a247a1
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-22 18:19:57 +0800

    Update Scale_cordon.sh

--- exam/single/Scale_cordon.sh
+++ exam/single/Scale_cordon.sh
@@ -1,2 +1,4 @@
+cluster=$(kubectl config view |grep 'cluster: ' | cut -d ':' -f 2)
+
 curl -s 'https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/single/Deployment-Scale.sh' | bash
 curl -s 'https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/single/cordon_drain.sh'|bash
