d005fa3d86ae337d55870014abcd3a143258eaf6
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-23 20:27:50 +0800

    Update clearenv.sh

--- exam/clearenv.sh
+++ exam/clearenv.sh
@@ -55,7 +55,8 @@ kubectl uncordon ${cluster}-worker2
 ## Trobleshooting - kubelet 故障(2%?)
 which ssh sshd &>/dev/null
 [[ $? != 0 ]] && apt update && apt install -y ssh
-apt install sudo
+which sudo &>/dev/null
+[[ $? != 0 ]] && apt install sudo
 echo -e \root\\nroot\\n| passwd root &>/dev/null
 systemctl stop kubelet
 
