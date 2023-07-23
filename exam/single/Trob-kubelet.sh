75075ec418981488aea0411b54990d6630da19f3
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-23 19:58:07 +0800

    Update Trob-kubelet.sh

--- exam/single/Trob-kubelet.sh
+++ exam/single/Trob-kubelet.sh
@@ -5,6 +5,6 @@ apt install sudo
 echo -e \root\\nroot\\n| passwd root &>/dev/null
 systemctl stop kubelet
 grep wk8s-node-0 /etc/hosts &>/dev/null
-[ $? != '0' ] && echo "127.0.0.1       localhost wk8s-node-0" | tee -a /etc/hosts 
+[ $? != 0 ] && echo "127.0.0.1       localhost wk8s-node-0" | tee -a /etc/hosts 
 grep wk8s-node-1 /etc/hosts &>/dev/null
-[ $? != '0' ] && echo "127.0.0.1       localhost ek8s-node-1" | tee -a /etc/hosts 
+[ $? != 0 ] && echo "127.0.0.1       localhost ek8s-node-1" | tee -a /etc/hosts 
