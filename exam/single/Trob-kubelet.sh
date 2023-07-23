2d2f9856598d16e63e332fe1f221c67bd617d24e
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-23 18:55:48 +0800

    Create Trob-kubelet.sh

---
+++ exam/single/Trob-kubelet.sh
@@ -0,0 +1,8 @@
+## Trobleshooting - kubelet 故障(2%?)
+which ssh &>/dev/null
+[ $? != 0 ] && apt update && apt install -y ssh
+apt install sudo
+echo -e \root\\nroot\\n| passwd root &>/dev/null
+
+echo "127.0.0.1       localhost wk8s-node-0" | tee -a /etc/hosts 
+echo "127.0.0.1       localhost ek8s-node-1" | tee -a /etc/hosts 
