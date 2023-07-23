63d57828edfbdd29c255533d214affb9831837c2
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-23 20:34:48 +0800

    Update Deployment

--- exam/single/Deployment-Scale.sh
+++ exam/single/Deployment-Scale.sh
@@ -3,5 +3,3 @@
 kubectl delete deploy presentation
 kubectl delete deploy loadbalance
 
-kubectl create deployment presentation --image=registry.k8s.io/echoserver:1.10 --replicas=0
-kubectl create deployment loadbalance --image=registry.k8s.io/echoserver:1.10 --replicas=0
