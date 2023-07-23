30e4018c869a49c9a102f5ae29a1a407e1d886d9
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-23 11:31:12 +0800

    Update clearenv.sh

--- exam/clearenv.sh
+++ exam/clearenv.sh
@@ -47,8 +47,8 @@ kubectl create -f https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/y
 kubectl delete deploy presentation 2> /dev/null;
 kubectl delete deploy loadbalance 2> /dev/null;
 
-kubectl create deployment presentation --image=registry.k8s.io/echoserver:1.10 --replicas=0 2> dev/null;
-kubectl create deployment loadbalance --image=registry.k8s.io/echoserver:1.10 --replicas=0 2> dev/null;
+kubectl create deployment presentation --image=registry.k8s.io/echoserver:1.10 --replicas=0 2>/dev/null;
+kubectl create deployment loadbalance --image=registry.k8s.io/echoserver:1.10 --replicas=0 2>/dev/null;
 
 ## cordon & drain (4%)
 kubectl uncordon ${cluster}-worker
