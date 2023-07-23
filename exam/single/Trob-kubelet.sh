b3465cf0745aa9dc4481eae1d653fb2e961b84ad
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-23 20:04:00 +0800

    Update Trob-kubelet.sh

--- exam/single/Trob-kubelet.sh
+++ exam/single/Trob-kubelet.sh
@@ -1,6 +1,6 @@
 ## Trobleshooting - kubelet 故障(2%?)
 which ssh &>/dev/null
-[ $? -ne 0 ] && apt update && apt install -y ssh
+[ $? != 0 ] && apt update && apt install -y ssh
 apt install sudo
 echo -e \root\\nroot\\n| passwd root &>/dev/null
 systemctl stop kubelet
