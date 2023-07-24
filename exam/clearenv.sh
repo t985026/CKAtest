d1bbc4639a6cda25d60ab85389ce9d8ce68113bb
f0603026 <50320568+f0603026@users.noreply.github.com>
2023-07-24 18:06:38 +0800

    Update clearenv.sh

--- exam/clearenv.sh
+++ exam/clearenv.sh
@@ -14,6 +14,13 @@ alias ll='ls -alh'"| tee -a /root/.bashrc
 
 rm *.yaml &>/dev/null
 echo 'remove all yaml' 
+## Deployment - Scale (4%) 前置
+kubectl delete deploy presentation 2> /dev/null;
+kubectl delete deploy loadbalancer 2> /dev/null;
+## Service (7%)
+kubectl delete deploy front-end 2>/dev/null;
+kubectl delete svc front-end-svc 2>/dev/null;
+
 ### Pod Logs (5%)
 [ ! -d /opt/KUTR00101 ] && mkdir -p /opt/KUTR00101
 [ -f /opt/KUTR00101/bar ] && rm /opt/KUTR00101/*
@@ -71,9 +78,6 @@ kubectl run cpu1 --image=quay.io/cloudwalker/sre.base --restart=Never --labels=n
 kubectl create -f https://raw.githubusercontent.com/f0603026/CKAtest/main/exam/yaml/F6-pod-sidecar.yaml
 
 ## Deployment 應用 - Scale (4%)
-kubectl delete deploy presentation 2> /dev/null;
-kubectl delete deploy loadbalancer 2> /dev/null;
-
 kubectl create deployment presentation --image=registry.k8s.io/echoserver:1.10 --replicas=0 2>/dev/null;
 kubectl create deployment loadbalancer --image=registry.k8s.io/echoserver:1.10 --replicas=0 2>/dev/null;
 
@@ -94,8 +98,6 @@ kubectl create namespace my-app 2>/dev/null;
 kubectl create namespace echo 2>/dev/null;
 
 ## Service (7%)
-kubectl delete deploy front-end 2>/dev/null;
-kubectl delete svc front-end-svc 2>/dev/null;
 kubectl create deployment front-end --image=quay.io/cloudwalker/nginx 2>/dev/null;
 ### RBAC (Role-based Access Control) (4%) 
 kubectl delete ns app-team1 2>/dev/null;
